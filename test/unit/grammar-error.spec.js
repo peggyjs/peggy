904bc338b3a13bbaf67c5d2ec92bb11c4e9dbe74
Joe Hildebrand <joe-github@cursive.net>
2023-03-07 14:06:13 -0700

    Handle null and undefined explicitly

--- test/unit/grammar-error.spec.js
+++ test/unit/grammar-error.spec.js
@@ -147,6 +147,15 @@ error: message
 warning: Warning message
  at foo.peggy:1:6
  at foo.peggy:1:6: Warning Subinfo`);
+
+          expect(e.format([{ source: null, text: undefined }])).to.equal(`\
+error: message
+ at foo.peggy:1:1
+ at foo.peggy:1:6: Subinfo
+
+warning: Warning message
+ at foo.peggy:1:6
+ at foo.peggy:1:6: Warning Subinfo`);
         });
 
         it("with source", () => {
